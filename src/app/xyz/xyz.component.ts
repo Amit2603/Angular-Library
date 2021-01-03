69c34fee4cfecc15d95e47a1beacbcb510bcd28c
Amit Pokharna <amit.pokharna@confluxsys.com>
2021-01-03 16:43:28 +0530

    dependency update

--- src/app/xyz/xyz.component.ts
+++ src/app/xyz/xyz.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-xyz',
   templateUrl: './xyz.component.html',
-  styleUrls: ['./xyz.component.scss']
+  styleUrls: ['./xyz.component.css']
 })
 export class XyzComponent implements OnInit {
 
